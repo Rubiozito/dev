adc6f0ae530af39eccaa921406172146d0d3ab58
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-06-08 13:10:11 -0300

    exemplo conexão back front

---
+++ back/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const tarefas = [];
+let id = 1;
+
+app.get("/tarefas", (req, res) => {
+  res.json(tarefas);
+});
+
+app.post("/tarefas", (req, res) => {
+  const texto = req.body.texto;
+  const tarefa = { id, texto };
+  tarefas.push(tarefa);
+  id++;
+  res.status(201).json(tarefa);
+});
+
+const PORT = 50000;
+app.listen(PORT, () => {
+  console.log(`Executando. Porta ${PORT}`);
+});
